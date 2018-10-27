ea454604a01301dc28e3a7e46b9f57139ab40ac0
Loyola User <confer15@luc.edu>
2018-10-27 11:41:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("h1").click(function(){
+        $("p").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".mushroom1").click(function(){
+    $(".mushroom1").effect("shake");
+    });
+});
+
